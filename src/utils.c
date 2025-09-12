faf2fc417bf59d8451dd4a1148e035eefcc90a34
dev#Mikami <121209531+sskyNS@users.noreply.github.com>
2025-09-12 14:45:35 +0800

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -5,16 +5,26 @@
  */
 #include <string.h>
 #include <errno.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #include "utils.h"
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <direct.h>
+    #define MKDIR(path, mode) _mkdir(path)
+#else
+    #include <sys/stat.h>
+    #include <sys/types.h>
+    #define MKDIR(path, mode) mkdir(path, mode)
+#endif
 
 static int _mkpath(char* file_path, mode_t mode) {
     for (char* p = strchr(file_path + 1, '/'); p; p = strchr(p + 1, '/')) {
         *p = '\0';
-        if (mkdir(file_path, mode) == -1) {
+        
+        if (MKDIR(file_path, mode) == -1) {
             if (errno != EEXIST) {
                 *p = '/';
                 return -1;
@@ -27,11 +37,22 @@ static int _mkpath(char* file_path, mode_t mode) {
 
 int mkpath(mode_t mode, const char* fmt, ...) {
     char * path = malloc(FILENAME_MAX);
+    if (path == NULL) {
+        return -1;
+    }
     
     va_list args;
     va_start(args, fmt);
     vsnprintf(path, FILENAME_MAX, fmt, args);
     va_end(args);
+
+    #if defined(_WIN32) || defined(_WIN64)
+    for (char* p = path; *p; p++) {
+        if (*p == '/') {
+            *p = '\\';
+        }
+    }
+    #endif
     
     int ret = _mkpath(path, mode);
     free(path);
